deb69a12f3517aa2b0edcec3d8baf4e6f68ac302
Xavier <business.xsantos@gmail.com>
2022-02-16 14:08:57 -0800

    prod

--- src/ideas/Builder12/layers/basis/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./classes/world";
-export * from "./classes/idea";
-export * from "./types/world";
-export * from "./types/idea";
-export * from "./visual/VisualIdea";
-export * from "./visual/VisualWorld";
--- src/ideas/NFTGateway/components/NFTChecker.tsx
+++ src/ideas/NFTGateway/components/NFTChecker.tsx
@@ -2,11 +2,9 @@ import React, { useEffect } from "react";
 import { Text, Interactable, Image, Floating } from "spacesvr";
 import { useMoralis, useMoralisCloudFunction } from "react-moralis";
 
-import Dome from "../../Dome";
-import FloatingOrb from "../../FloatingOrb";
 import Media from "ideas/MediaIdea";
 
-const NFTChecker = (props: { address: any; chain: any; }) => {
+const NFTChecker = (props: { address: any; chain: any }) => {
   const address = props.address;
   const chain = props.chain;
   const email = "ryan@dropparty.io";
@@ -35,151 +33,88 @@ const NFTChecker = (props: { address: any; chain: any; }) => {
 
   return (
     <group name="nft-gateway">
-      {!(isOwner == 2) && (
-        <group>
-          <Dome />
-        </group>
-      )}
       {!isAuthenticated && !isAuthenticating && (
         <group>
-          <Floating height={0.05} speed={1.5}>
-            <Interactable onClick={() => authenticate()}>
-              <Text
-                text="METAMASK"
-                vAlign="center" // vertical align relative to the y component
-                hAlign="center" // horizontal align relative to the x component
-                size={0.6} // scale
-                position={[-0.9, 0, 0.01]}
-                color="white" // color
-              />
-              <Media
-                media="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
-                framed={true}
-                position={[-0.9, 0, 0.05]}
-                scale={0.5}
-              />
-            </Interactable>
-            <Interactable
-              onClick={() =>
-                authenticate({
-                  provider: "walletconnect",
-                  signingMessage: "Sign in to DropParty",
-                })
-              }
-            >
-              <Text
-                text="WALLETCONNECT"
-                vAlign="center" // vertical align relative to the y component
-                hAlign="center" // horizontal align relative to the x component
-                size={0.6} // scale
-                position={[0.7, 0, 0.03]}
-                color="black" // color
-              />
-              <Media
-                media="https://i.imgur.com/6W0yKmv.png"
-                framed={true}
-                position={[0.7, 0, 0.05]}
-                scale={0.5}
-              />
-            </Interactable>
-          </Floating>
+          <Interactable onClick={() => authenticate()}>
+            <Text
+              text="METAMASK"
+              vAlign="center"
+              hAlign="center"
+              size={0.35}
+              position={[-0.4, 0, 0.04]}
+              color="white"
+            />
+            <Media
+              media="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
+              framed={true}
+              frameColor="black"
+              position={[-0.4, 0, 0.05]}
+              scale={0.25}
+              collision={false}
+            />
+          </Interactable>
+          <Interactable
+            onClick={() =>
+              authenticate({
+                provider: "walletconnect",
+                signingMessage: "Sign in to DropParty",
+              })
+            }
+          >
+            <Text
+              text="WALLETCONNECT"
+              vAlign="center"
+              hAlign="center"
+              size={0.35} // scale
+              position={[0.4, 0, 0.03]}
+              color="black" // color
+            />
+            <Media
+              media="https://i.imgur.com/6W0yKmv.png"
+              framed={true}
+              frameColor="black"
+              position={[0.4, 0, 0.05]}
+              scale={0.25}
+              collision={false}
+            />
+          </Interactable>
         </group>
+      )}
+      {(isAuthenticating || !isAuthenticated) && (
+        <Media
+          media="https://t3.ftcdn.net/jpg/02/88/89/90/360_F_288899075_TV8KKBLTOnG0Dby3IC61UCUeNiBK0puK.jpg"
+          framed
+          frameColor="black"
+          collision
+          scale={1.5}
+        />
       )}
       {isAuthenticating && (
         <Text
           text="Please wait..."
-          vAlign="center" // vertical align relative to the y component
-          hAlign="center" // horizontal align relative to the x component
-          size={1.1} // scale
+          vAlign="center"
+          hAlign="center"
+          size={1.1}
           position={[0, 0, -0.05]}
-          color="red" // color
+          color="red"
         />
       )}
 
       {/* Debug Tools */}
-      {isOwner == 2 && isAuthenticated && Debugging && (
-        <group>
-          <FloatingOrb />
-        </group>
-      )}
-      {isAuthenticated && Debugging && (
-        <group>
-          <Text
-            text={user?.get("ethAddress")}
-            vAlign="center" // vertical align relative to the y component
-            hAlign="center" // horizontal align relative to the x component
-            size={1} // scale
-            position={[0, 0.3, -0.05]}
-            color="black" // color
-          />
-        </group>
-      )}
       {!isAuthenticating && isAuthenticated && Debugging && (
         <Interactable onClick={() => logout()}>
           <Text
             text="DISCONNECT"
-            vAlign="center" // vertical align relative to the y component
-            hAlign="center" // horizontal align relative to the x component
-            size={1.1} // scale
+            vAlign="center"
+            hAlign="center"
+            size={1.1}
             position={[-0.9, 0, -0.05]}
-            color="red" // color
+            color="red"
           />
         </Interactable>
       )}
-      {isAuthenticated && Debugging && (
-        <group>
-          <Text
-            text={"Owns NFT:"}
-            vAlign="center" // vertical align relative to the y component
-            hAlign="center" // horizontal align relative to the x component
-            size={1} // scale
-            position={[0, -0.3, -0.05]}
-            color="black" // color
-          />
-          <Text
-            text={isOwner == 2 || isOwner == 3 ? "Yes" : "No"}
-            vAlign="center" // vertical align relative to the y component
-            hAlign="center" // horizontal align relative to the x component
-            size={1} // scale
-            position={[0, -0.42, -0.05]}
-            color="yellow" // color
-          />
-        </group>
-      )}
-      {isAuthenticated && Debugging && (
-        <group>
-          <Text
-            text={"Subscribed:"}
-            vAlign="center" // vertical align relative to the y component
-            hAlign="center" // horizontal align relative to the x component
-            size={1} // scale
-            position={[0, 0.43, -0.05]}
-            color="black" // color
-          />
-          <Text
-            text={isOwner == 2 || isOwner == 1 ? "Yes" : "No"}
-            vAlign="center" // vertical align relative to the y component
-            hAlign="center" // horizontal align relative to the x component
-            size={1} // scale
-            position={[0.5, 0.43, -0.05]}
-            color="yellow" // color
-          />
-        </group>
-      )}
-      {isAuthenticated && Debugging && (
-        <Image
-          src="https://t3.ftcdn.net/jpg/02/88/89/90/360_F_288899075_TV8KKBLTOnG0Dby3IC61UCUeNiBK0puK.jpg"
-          size={3}
-          framed
-        />
-      )}
-      <Media
-      media="https://t3.ftcdn.net/jpg/02/88/89/90/360_F_288899075_TV8KKBLTOnG0Dby3IC61UCUeNiBK0puK.jpg"
-      framed
-      frameColor="black"
-      collision />
     </group>
   );
-}
+};
 
 export default NFTChecker;
