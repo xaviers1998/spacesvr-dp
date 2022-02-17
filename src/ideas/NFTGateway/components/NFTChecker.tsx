ae3e9e36608ecc007d97cc7f771258e9130dc7f7
Xavier <business.xsantos@gmail.com>
2022-02-16 17:17:46 -0800

    production

--- src/ideas/NFTGateway/components/NFTChecker.tsx
+++ src/ideas/NFTGateway/components/NFTChecker.tsx
@@ -1,17 +1,18 @@
 import React, { useEffect } from "react";
-import { Text, Interactable, Image, Floating } from "spacesvr";
+import { Text, Interactable } from "spacesvr";
 import { useMoralis, useMoralisCloudFunction } from "react-moralis";
 
 import Media from "ideas/MediaIdea";
 
-const NFTChecker = (props: { address: any; chain: any }) => {
+const NFTChecker = (props: { address: string; chain: string; media: string; }) => {
   const address = props.address;
   const chain = props.chain;
+  const media = props.media;
   const email = "ryan@dropparty.io";
-  const Debugging = true;
 
   const { authenticate, isAuthenticated, user, logout, isAuthenticating } =
     useMoralis();
+
   const { fetch, data: isOwner } = useMoralisCloudFunction(
     "isNFTOwner",
     {
@@ -22,33 +23,20 @@ const NFTChecker = (props: { address: any; chain: any }) => {
     { autoFetch: false }
   );
 
-  // isOwner Values
-  // 1 --- Subscription: ACTIVE | NFT Owner: NO
-  // 2 --- Subscription: ACTIVE | NFT Owner: YES
-  // 3 --- Subscription: INACTIVE | NFT Owner: YES
-
   useEffect(() => {
     fetch();
-  }, [user, props]);
+  }, [user, props, fetch]);
 
   return (
     <group name="nft-gateway">
       {!isAuthenticated && !isAuthenticating && (
         <group>
           <Interactable onClick={() => authenticate()}>
-            <Text
-              text="METAMASK"
-              vAlign="center"
-              hAlign="center"
-              size={0.35}
-              position={[-0.4, 0, 0.04]}
-              color="white"
-            />
             <Media
               media="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
               framed={true}
               frameColor="black"
-              position={[-0.4, 0, 0.05]}
+              position={[-0.15, 0, 0.05]}
               scale={0.25}
               collision={false}
             />
@@ -61,28 +49,20 @@ const NFTChecker = (props: { address: any; chain: any }) => {
               })
             }
           >
-            <Text
-              text="WALLETCONNECT"
-              vAlign="center"
-              hAlign="center"
-              size={0.35} // scale
-              position={[0.4, 0, 0.03]}
-              color="black" // color
-            />
             <Media
               media="https://i.imgur.com/6W0yKmv.png"
               framed={true}
               frameColor="black"
-              position={[0.4, 0, 0.05]}
+              position={[0.15, 0, 0.05]}
               scale={0.25}
               collision={false}
             />
           </Interactable>
         </group>
       )}
-      {(isOwner !== 2) && (
+      {isOwner !== 2 && (
         <Media
-          media="https://t3.ftcdn.net/jpg/02/88/89/90/360_F_288899075_TV8KKBLTOnG0Dby3IC61UCUeNiBK0puK.jpg"
+          media={media}
           framed
           frameColor="black"
           collision
@@ -94,23 +74,22 @@ const NFTChecker = (props: { address: any; chain: any }) => {
           text="Please wait..."
           vAlign="center"
           hAlign="center"
-          size={1.1}
-          position={[0, 0, -0.05]}
+          size={1}
           color="red"
         />
       )}
 
       {/* Debug Tools */}
-      {!isAuthenticating && isAuthenticated && Debugging && (
+      {!isAuthenticating && isAuthenticated && true && (
         <Interactable onClick={() => logout()}>
-          <Text
-            text="DISCONNECT"
-            vAlign="center"
-            hAlign="center"
-            size={1.1}
-            position={[-0.9, 0, -0.05]}
-            color="red"
-          />
+          <Media
+              media="https://i.imgur.com/Z22YaYG.jpg"
+              framed={true}
+              frameColor="black"
+              position={[0, 0, 0.05]}
+              scale={0.25}
+              collision={false}
+            />
         </Interactable>
       )}
     </group>
--- src/ideas/NFTGateway/index.tsx
+++ src/ideas/NFTGateway/index.tsx
@@ -1,19 +1,22 @@
 import { MoralisProvider } from "react-moralis";
 import NFTChecker from "./components/NFTChecker";
 
-export type InitializerProps = {
+export type NFTGatewayProps = {
     address: string;
     chain: string;
+    media: string;
   };
 
-export default function Initializer(props: InitializerProps) {
+export default function NFTGateway(props: NFTGatewayProps) {
     const {
         address = "0xda05ce4a985d2aa058382877e08fb38bd649fb60",
         chain = "ETH",
+        media = "https://t3.ftcdn.net/jpg/02/88/89/90/360_F_288899075_TV8KKBLTOnG0Dby3IC61UCUeNiBK0puK.jpg"
       } = props;
+      
   return (
-    <MoralisProvider appId="lDDXYagREnknCf9WxDm994rB9YcHiEfk767ehbna" serverUrl="https://ckb11ejzq8dp.grandmoralis.com:2053/server" initializeOnMount={false}>
-        <NFTChecker address={address} chain={chain} />
+    <MoralisProvider appId="lDDXYagREnknCf9WxDm994rB9YcHiEfk767ehbna" serverUrl="https://ckb11ejzq8dp.grandmoralis.com:2053/server" initializeOnMount>
+        <NFTChecker address={address} chain={chain} media={media} />
     </MoralisProvider>
   );
 }
